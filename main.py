1567ef5690706c901d00826908321a41f1b28dde
James Grogan <james.grogan2@gmail.com>
2021-12-20 17:47:52 +0000

    Add token caching

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import pkce
 from dotenv import load_dotenv
 import os
 import urllib.parse
+import datetime
+import json
+import os.path
+from os import path
 
 load_dotenv()
 
@@ -15,6 +19,15 @@ CLIENT_ID = os.environ.get("CLIENT_ID")
 REDIRECT_URL = os.environ.get("REDIRECT_URL")
 SCOPE = os.environ.get("SCOPE")
 TOKEN_URL = os.environ.get("TOKEN_URL")
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+folder_exists = os.path.exists(dir_path + "/.cache")
+
+if not folder_exists:
+    os.makedirs(dir_path + "/.cache")
+
+cache_file = dir_path + "/.cache/oauth.json"
 
 class HTTPServerHandler(BaseHTTPRequestHandler):
     def __init__(self, request, address, server):
@@ -65,7 +78,28 @@ def get_access_token():
             'client_id': CLIENT_ID,
             'code_verifier': code_verifier
         })
-    print(r.json()['access_token'])
+    data = r.json()
+    expiration_time =  datetime.datetime.now() + datetime.timedelta(seconds=data['expires_in'])
+    data['expiration_date'] =   int(expiration_time.timestamp())
+
+    with open(cache_file, "w+") as outfile:
+        json.dump(data, outfile)
+
+    return data
 
  
-get_access_token()
+if path.exists(cache_file):
+    print("Exists")
+    #Reading cache
+    with open(cache_file, "r") as infile:
+        access_token = json.load(infile)
+    if int(datetime.datetime.now().timestamp()) > access_token['expiration_date']:
+        print("Out of date")
+        #Token expired, get new
+        access_token = get_access_token()
+else:
+    #No cached value, get and cache
+    access_token = get_access_token()
+
+r = requests.get("https://graph.microsoft.com/v1.0/sites/fc750661-1d01-4881-8d31-2fc88ef07d90/lists" + "/1aef1485-f4be-4d05-8ca8-e4f7c26266fa/items?expand=fields", headers={'Authorization': "Bearer " + access_token["access_token"]})
+
